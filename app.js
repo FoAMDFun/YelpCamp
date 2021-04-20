d0b5e48477f677942f3bc8de8e1da91b6eb10cb8
FoAMDFun <foamdfun@gmail.com>
2021-04-20 16:19:56 +0200

    Camplist done

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
-const express = require('express');
-const app = express();
 const port = 3000;
+const dataBaseURL = "mongodb://localhost:27017/yelp-camp";
 
-app.get('/', (req, res) => {
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
+
+mongoose.connect(dataBaseURL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// routing
+app.get("/", (req, res) => {
     // It gets the root query
-    res.send('Hello from Yelp Camp!');
-})
+    res.render("home");
+});
+
+app.get("/campgrounds", async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", { campgrounds });
+});
 
 app.listen(port, () => {
     console.log(`Serving on port ${port}`);
-})
+});
---
+++ seeds/index.js
@@ -0,0 +1,44 @@
+const port = 3000;
+const dataBaseURL = "mongodb://localhost:27017/yelp-camp";
+
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+const Campground = require("../models/campground");
+
+mongoose.connect(dataBaseURL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+// Clear db
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+        });
+
+        await camp.save();
+    }
+};
+
+console.log(
+    "clearing and rewriting of the database with random elements has started..."
+);
+seedDB().then(() => {
+    mongoose.connection.close();
+});
+console.log("done");
