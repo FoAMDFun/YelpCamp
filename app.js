ebd8a6cc8817c528a307456e1d8876123cc6120b
FoAMDFun <foamdfun@gmail.com>
2021-05-21 17:32:28 +0200

    Deploy 4

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
-const dataBaseURL = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
 if (process.env.NODE_ENV !== "production") {
     require("dotenv").config();
 }
+
+const dataBaseURL = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
 const port = process.env.PORT || 80;
 const secret = process.env.SECRET || "thisshouldbeabettersecret";
 const express = require("express");
